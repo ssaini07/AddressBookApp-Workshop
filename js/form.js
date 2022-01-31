570643f3f3f8074524e994fa5bc64a4c7f5ff638
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-31 22:26:18 +0530

    Added : Successfully tested form validations on name, address, and phone number

---
+++ js/form.js
@@ -0,0 +1,40 @@
+//UC => 4 Added form validations
+window.addEventListener('DOMContentLoaded', (event) => {
+    validatingInputs();
+})
+
+function validatingInputs() {
+    const name = document.querySelector('#name');
+    const phone = document.querySelector('#phone');
+    const address = document.querySelector('#address');
+    const nameError = document.querySelector('.text-error');
+    const phoneError = document.querySelector('.phone-error');
+    const addressError = document.querySelector('.address-error');
+
+    name.addEventListener('input', function() {
+        const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name.value)) {
+            nameError.textContent = "";
+        } else {
+            nameError.textContent = "Name is incorrect";
+        }
+    });
+
+    phone.addEventListener('input', function() {
+        const phoneRegex = RegExp('^[0-9]{10}$');
+        if (phoneRegex.test(phone.value)) {
+            phoneError.textContent = "";
+        } else {
+            phoneError.textContent = "Phone number is incorrect";
+        }
+    });
+
+    address.addEventListener('input', function() {
+        const addressRegex = RegExp('^[a-zA-Z0-9\s,.-: ]{3,}$');
+        if (addressRegex.test(address.value)) {
+            addressError.textContent = "";
+        } else {
+            addressError.textContent = "Address is incorrect";
+        }
+    });
+}
