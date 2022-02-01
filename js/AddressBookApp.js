7ccbb51591c61bd3788be1b1e9b9c31139d567e8
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-02-01 12:15:02 +0530

    Added : Perfomring number of operations specified in the next use cases

---
+++ js/AddressBookApp.js
@@ -0,0 +1,51 @@
+//UC => 6
+class AddressBookApp {
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+    set phone(phone) {
+        this._phone = phone;
+    }
+
+    get address() {
+        return this._address;
+    }
+    set address(address) {
+        this._address = address;
+    }
+
+    get city() {
+        return this._city;
+    }
+    set city(city) {
+        this._city = city;
+    }
+
+    get state() {
+        return this._state;
+    }
+    set state(state) {
+        this._state = state;
+    }
+
+    get zipcode() {
+        return this._zipcode;
+    }
+    set zipcode(zipcode) {
+        this._zipcode = zipcode;
+    }
+}
